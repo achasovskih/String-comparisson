8b025e48061df1f602afbf30c755726d30bdb118
achasovskih <94637022+achasovskih@users.noreply.github.com>
2022-01-16 20:49:36 +0300

    TestApp
    My test application

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Открыть файл 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Расчитать разницу файлов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(43, 195);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(244, 316);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(323, 195);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(253, 316);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(611, 195);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(253, 316);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 631);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Test Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        enum diffType : int
+        {
+            OldLine = 1,
+            ModifiedLine,
+            DeletedLine
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            var fileInfo = OpenFileDlg();
+
+            if (fileInfo == null)
+                return;
+
+            richTextBox1.AppendText(File.ReadAllText(fileInfo.FullName));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            var fileInfo = OpenFileDlg();
+
+            if (fileInfo == null)
+                return;
+
+            richTextBox2.AppendText(File.ReadAllText(fileInfo.FullName));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Clear();
+
+            string file1 = this.richTextBox1.Text;
+            string file2 = this.richTextBox2.Text;
+
+            // simple optimization
+            if (file1.GetHashCode() == file2.GetHashCode())
+            {
+                this.richTextBox3.SelectionColor = Color.Green;
+                this.richTextBox3.AppendText(file1);
+                return;
+            }
+
+            if (file2.Length == 0 && file1.Length > 0)
+            {
+                this.richTextBox3.SelectionColor = Color.Red;
+                this.richTextBox3.AppendText(file1);
+                return;
+            }
+
+            if (file1.Length == 0 && file2.Length > 0)
+            {
+                this.richTextBox3.SelectionColor = Color.Orange;
+                this.richTextBox3.AppendText(file2);
+                return;
+            }
+
+            var list1 = file1.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+            var list2 = file2.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+
+            var diffs = (list1.Length >= list2.Length) ? Diff(list1, list2, true)
+                                                       : Diff(list2, list1, false);
+
+            foreach (var d in diffs)
+            {
+
+                switch (d.Type)
+                {
+                    case diffType.OldLine:
+                        this.richTextBox3.SelectionColor = Color.Green;
+                        break;
+                    case diffType.ModifiedLine:
+                        this.richTextBox3.SelectionColor = Color.Orange;
+                        break;
+                    case diffType.DeletedLine:
+                        this.richTextBox3.SelectionColor = Color.Red;
+                        break;
+                    default:
+                        break;
+                };
+
+                this.richTextBox3.AppendText(d.Value + "\n");
+            }
+        }
+
+        private FileInfo OpenFileDlg()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "txt | *.txt";
+
+            DialogResult result = dlg.ShowDialog();
+
+            string filePath;
+
+            if (result == DialogResult.OK)
+            {
+                filePath = dlg.FileName;
+                return new FileInfo(filePath);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file1">Larger file</param>
+        /// <param name="file2"></param>
+        /// <param name="isSrc">flag if file1 para is source of target file</param>
+        /// <returns></returns>
+        private List<(string Value, diffType Type)> Diff(string[] list1, string[] list2, bool isSrc)
+        {
+            List<(string, diffType)> diffList = new List<(string, diffType)>(list1.Length);
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                if (list2.Contains(list1[i]))
+                {
+                    diffList.Add((list1[i], diffType.OldLine));
+                }
+                else
+                {
+                    diffList.Add((list1[i], (isSrc ? diffType.DeletedLine : diffType.ModifiedLine)));
+
+                    float highestMutchCoff = 0;
+                    float matchCoff = 0;
+                    int index = 0;
+
+                    for (int j = 0; j < list2.Length; j++)
+                    {
+                        matchCoff = TanimotoCoff(list1[i], list2[j]);
+
+                        if (highestMutchCoff < matchCoff)
+                        {
+                            highestMutchCoff = matchCoff;
+                            index = j;
+                        }
+
+                        if (highestMutchCoff < 0.9f && highestMutchCoff > 0.5f && !list1.Contains(list2[index]))
+                        {
+                            diffList.Add((list2[index], diffType.ModifiedLine));
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach (var item in list2) 
+            {
+                if (diffList.Find(x => x.Item1 == item) == default)
+                {
+                    diffList.Add((item, diffType.ModifiedLine));
+                }
+            }
+
+            return diffList;
+        } 
+
+        private float TanimotoCoff(string s1, string s2)
+        {
+            int a = s1.Length, b = s2.Length;
+            float c = 0f;
+
+            foreach (var sym in s1)
+            {
+                if (s2.Contains(sym))
+                {
+                    c += 1;
+                }
+            }
+            return c / (a + b - c);
+        }
+
+    }
+}
